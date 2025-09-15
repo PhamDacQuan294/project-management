6bf1e654a444e138bf78a2079a148d8471536938
quan <quan93909@gmail.com>
2025-09-15 14:07:06 +0700

    Lấy dữ liệu topics ra

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -1,1 +1,17 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+
 // [GET] /topics/
+export const topics = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+
+  console.log(topics);
+  
+  if (topics) {
+    res.render("client/pages/topics/index", {
+      topics: topics
+    })
+  }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// Client Routes
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
