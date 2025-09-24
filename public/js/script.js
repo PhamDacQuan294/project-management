196e00e46a419b5346debe44b4a7443c21732572
binh <binlun411@gmail.com>
2025-09-24 15:23:21 +0700

    Làm nút Bài hát yêu thích trang chi tiết bài hát 2

--- public/js/script.js
+++ public/js/script.js
@@ -33,4 +33,61 @@ if(aplayer) {
   });
 
 }
-// End Aplayergit
+// End Aplayer
+
+// Button Like
+const buttonLike = document.querySelector("[button-like]");
+if(buttonLike) {
+  buttonLike.addEventListener("click", () => {
+    const idSong = buttonLike.getAttribute("button-like");
+    const isActive = buttonLike.classList.contains("active");
+
+    const typeLike = isActive ? "dislike" : "like";
+
+    const link = `/songs/like/${typeLike}/${idSong}`;
+
+    const option = {
+      method: "PATCH"
+    }
+
+    fetch(link, option)
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == 200) {
+            const span = buttonLike.querySelector("span");
+            span.innerHTML = `${data.like} thich`;
+        }
+
+        buttonLike.classList.toggle("active");
+      });
+  });
+}
+// End Button Like
+
+// Button Favorite
+const buttonFavorite = document.querySelector("[button-favorite]");
+console.log(buttonFavorite);
+
+if(buttonFavorite) {
+  buttonFavorite.addEventListener("click", () => {
+    const idSong = buttonFavorite.getAttribute("button-favorite");
+    const isActive = buttonFavorite.classList.contains("active");
+
+    const typeFavorite = isActive ? "unfavorite" : "favorite";
+
+    const link = `/songs/favorite/${typeFavorite}/${idSong}`;
+
+    const option = {
+      method: "PATCH"
+    }
+
+    fetch(link, option)
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == 200) {
+          buttonFavorite.classList.toggle("active");
+        }
+      });
+  });
+}
+// End Button Favorite
--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -7,4 +7,6 @@ router.get("/:slugTopic", controller.list);
 
 router.get("/detail/:slugSong", controller.detail);
 
+router.patch("/favorite/:typeFavorite/:idSong", controller.favorite);
+
 export const songRoutes: Router = router;
